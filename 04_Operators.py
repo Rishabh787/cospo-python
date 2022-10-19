a21b949a6ee8606c836cfddec70b673eebb3b3d3
Rishabh787 <ris581978@gmail.com>
2022-10-19 22:28:53 +0530

    click for operators

--- 04_Operators.py
+++ 04_Operators.py
@@ -3,18 +3,16 @@
 # Arithmetic Operators :  Arithmetic operators are used with numeric values to perform common mathematical operations:
 #   [ Addition(+), Substraction(-), Multiplication(*), Division(/), Module(%), Exponentiation(**), Floor division(//) ]
 
-from re import M
 
+# x =4
+# y =5
+# z = x+y
+# print("addition of 4 and 5 = ", z)
 
-x =4
-y =5
-z = x+y
-print("addition of 4 and 5 = ", z)
-
-a = 5
-b = 3
-c = a-b
-print ("Substraction of 5 and 3 =", c)
+# a = 5
+# b = 3
+# c = a-b
+# print ("Substraction of 5 and 3 =", c)
 
 # as it is many more  
 
@@ -34,12 +32,12 @@ print ("Substraction of 5 and 3 =", c)
 # Logical Operators :  
 # Logical operators are used to combine conditional statements:
 
-m = 7
-n = 8
-o = 9
-print((m<n) and (n>m))
-print(m>n or  n<m)
-print(not m>n)
+# m = 7
+# n = 8
+# o = 9
+# print((m<n) and (n>m))
+# print(m>n or  n<m)
+# print(not m>n)
 
 
 
@@ -47,7 +45,19 @@ print(not m>n)
 # Identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
 
 
+# is 
+x = 4
+if(type(x) is int):
+    print("Correct");
+else:
+    print("Incorrect");
 
+# is not 
+u = 7.7
+if(type(u) is not int):
+    print("Correct");
+else:
+    print("Incorrect");
 
 
 #  Membership Operators :
