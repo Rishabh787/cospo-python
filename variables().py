09e839e35f747e28591d3e51182d7c8a167a1cf3
Rishabh787 <ris581978@gmail.com>
2022-10-19 12:03:57 +0530

    click for variables

---
+++ variables().py
@@ -0,0 +1,26 @@
+
+# Variable  #rules
+
+'''Variable must be start with: underscore(_), Letter(a-z). 
+   Symbols are not allowed to contain a variable.
+   Variables name are case sensitive: like; age, Age, AGe, agE, aGE, AgE  (These are all different variables name  (Like;).
+   Spaces are not  allowed to contain a variables.'''
+
+# Usuable Variables name
+
+_myname = "Mr.Singh"
+my_name = "Mr.Singh"
+my7name = "Mr.Singh"
+I_I_I_ = "Mr.Singh"
+MyName = "Mr.Singh"
+MY_NAME7 = "Mr.Singh"
+
+# Unusuable Variables name
+
+'''
+7myname = "hgniS"
+my name = "hgniS"
+my-name = "hgniS"
+&myname = "hgniS"
+
+'''
