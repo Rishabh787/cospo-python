7aa81502cb4c7b106c05958e68a65302bf787ebc
Rishabh787 <ris581978@gmail.com>
2022-10-20 23:53:56 +0530

    voiting program

---
+++ Program/voting.py
@@ -0,0 +1,8 @@
+
+#  Write a program to check whether an age is able to voting or not?
+
+age = int(input("Enter your age:"))
+if (age > 18):
+    print("This person is able for voting, because it's age is ", age)
+else:
+    print("This person is not able for voting, because it's age is ", age)
