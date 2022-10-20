9fe0283ff1447c0ebb845d83d8b8931bf2280428
Rishabh787 <ris581978@gmail.com>
2022-10-20 23:37:01 +0530

    Calculator program

---
+++ Program/Calculator.py
@@ -0,0 +1,19 @@
+
+
+print("Use 👉(1) for addition(+)")
+print("Use 👉(2) for subtraction(-)")
+print("Use 👉(3) for multiplication(*)")
+print("Use 👉(4) for division(/)")
+
+opr = int(input("Enter the operator ✍: "))
+value_1 = int(input("Enter First Number ✍: "))
+value_2 = int(input("Enter Second Number ✍: "))
+
+if (opr == 1):
+    print("Addition of first and second number ✍: ", value_1 + value_2)
+elif (opr == 2):
+    print("Subtraction of first and second number ✍: ", value_1 - value_2)
+elif (opr == 3):
+    print("Multiplication of first and second number ✍: ", value_1 * value_2)
+elif (opr == 4):
+    print("Division of first and second number ✍: ", value_1 // value_2)
