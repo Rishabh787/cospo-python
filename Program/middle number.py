ffb14130d1e4f8ed175b40af1a133cf80d569194
Rishabh787 <ris581978@gmail.com>
2022-10-21 13:46:52 +0530

    find middle number

---
+++ Program/middle number.py
@@ -0,0 +1,24 @@
+
+#  Write a program to find positive and negative number? 
+# 
+# z = int(input("Enter number:"))
+# if (z>0):
+#     print("positive", z)
+# elif (z<0):
+#     print("negative")
+# else:
+#     print("zero")
+
+
+#  Write a program to find middlr number between three numbers?
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = int(input("Enter third number:"))
+
+if (a>b and a<c):
+    print(a)
+elif (b>a and b<c):
+    print(b)
+elif (c>a and c<b):
+    print(c)
