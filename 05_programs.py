fabeb577c9475a33412a21b89de66bae7d49e6ec
Rishabh787 <ris581978@gmail.com>
2022-10-21 13:45:15 +0530

    update ✍

--- 05_programs.py
+++ 05_programs.py
@@ -24,27 +24,37 @@
 #  Write a program to accept electricity unit consuption and calculate total price at the rate 5 rupees unit. Give a discount of 10% of overr all bill? 
 
 
-unit = 100
-amt = 5
-total= unit * amt
+# unit = 100
+# amt = 5
+# total= unit * amt
 
-print("Amount of bill = ", total)
+# print("Amount of bill = ", total)
 
-dis = total *10/100
-pay= (total-dis)
-print("Discount for user: ",dis)
-print(f'Total amount is {total} after the discount need to pay only {pay}')
+# dis = total *10/100
+# pay= (total-dis)
+# print("Discount for user: ",dis)
+# print(f'Total amount is {total} after the discount need to pay only {pay}')
 
 
 #  Write a program to accept marks of 5 subjects and find total marks and percentage assuminng full marks as 100 in each subject.
 
-H = 98
-E = 89
-M = 99
-P = 89
-C = 97
+# H = 98
+# E = 89
+# M = 99
+# P = 89
+# C = 97
 
-Total = H+E+M+P+C
-print("Total marks out of 500: ", Total)
-percent = Total *100 / 500
-print("Total percentage of ", Total, "out of 500 is: ", percent, "%" )
+# Total = H+E+M+P+C
+# print("Total marks out of 500: ", Total)
+percent = 56
+print(percent)
+# print("Total percentage of ", Total, "out of 500 is: ", percent, "%" )
+
+if (percent<=50):
+    print("poor")
+elif(percent<=70):
+    print("Average")
+elif(percent<=100):
+    print("7 crore")
+else:
+    print("Padhai likhai karo 👎🏿 ")
