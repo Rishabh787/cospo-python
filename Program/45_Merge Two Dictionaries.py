69a8c65e03b63556940e580b2a3c3a0526616ffb
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-02-03 23:23:38 +0530

    add second method using opr.

--- Program/45_Merge Two Dictionaries.py
+++ Program/45_Merge Two Dictionaries.py
@@ -2,3 +2,10 @@ dict_1 = {1: 'a', 2: 'b'}
 dict_2 = {2: 'c', 4: 'd'}
 
 print(dict_1 | dict_2)
+
+
+Using the ** Operator
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+print({**dict_1, **dict_2})
