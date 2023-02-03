e5d58e7d1bc0a923ce59d3a9641a9abdeff32732
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-02-03 23:24:32 +0530

    add another method

--- Program/45_Merge Two Dictionaries.py
+++ Program/45_Merge Two Dictionaries.py
@@ -9,3 +9,13 @@ dict_1 = {1: 'a', 2: 'b'}
 dict_2 = {2: 'c', 4: 'd'}
 
 print({**dict_1, **dict_2})
+
+
+Using copy() and update()
+dict_1 = {1: 'a', 2: 'b'}
+dict_2 = {2: 'c', 4: 'd'}
+
+dict_3 = dict_2.copy()
+dict_3.update(dict_1)
+
+print(dict_3)
