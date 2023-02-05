49ca2d7b1663721ca1b9d07b6f86596c9e1e5c32
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-02-05 22:08:05 +0530

    Update 46_Access Index of a List Using for Loop.py

--- Program/46_Access Index of a List Using for Loop.py
+++ Program/46_Access Index of a List Using for Loop.py
@@ -2,3 +2,12 @@ my_list = [21, 44, 35, 11]
 
 for index, val in enumerate(my_list):
     print(index, val)
+    
+    
+# Start the indexing with non zero value
+# Example 2
+
+my_list = [21, 44, 35, 11]
+
+for index, val in enumerate(my_list, start=1):
+    print(index, val)
