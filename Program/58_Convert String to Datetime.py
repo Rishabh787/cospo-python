8951836863daa1ab200acc7889d0fbf0b5bcbc32
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-03-24 23:15:48 +0530

    Update 58_Convert String to Datetime.py

--- Program/58_Convert String to Datetime.py
+++ Program/58_Convert String to Datetime.py
@@ -6,3 +6,13 @@ datetime_object = datetime.strptime(my_date_string, '%b %d %Y %I:%M%p')
 
 print(type(datetime_object))
 print(datetime_object)
+
+
+Example 2 : Using dateutil module
+
+from dateutil import parser
+
+date_time = parser.parse("Mar 11 2011 11:31AM")
+
+print(date_time)
+print(type(date_time))
