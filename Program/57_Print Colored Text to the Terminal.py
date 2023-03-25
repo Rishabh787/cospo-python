7b59c170ebe014d55d647d4713ab0086a817da66
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-03-25 21:58:25 +0530

    add 2nd example

--- Program/57_Print Colored Text to the Terminal.py
+++ Program/57_Print Colored Text to the Terminal.py
@@ -1,1 +1,7 @@
-print('\x1b[38;2;5;86;243m' + 'Programiz' + '\x1b[0m')
+print('\x1b[38;2;5;86;243m' + 'Insanity' + '\x1b[0m')
+
+# Ex. 2: Using python module termcolor
+
+from termcolor import colored
+
+print(colored('Programiz', 'blue'))
