7677e45810e0935527aa3dbd0c76e60f5337dfde
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-03-29 23:22:54 +0530

    add 3rd methos

--- Program/48_Slice Lists.py
+++ Program/48_Slice Lists.py
@@ -2,8 +2,16 @@ my_list = [1, 2, 3, 4, 5]
 
 print(my_list[:])
 
+
 # Get all the Items After a Specific Position
 
 my_list = [1, 2, 3, 4, 5]
 
 print(my_list[2:])
+
+
+# Get all the Items Before a Specific Position
+
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[:2])
