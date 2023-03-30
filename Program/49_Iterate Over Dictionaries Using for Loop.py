07a7cfe26d02e34631b1b48fe82a93a95f4b6e98
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-03-30 23:42:47 +0530

    add example 2 and edit file name

---
+++ Program/49_Iterate Over Dictionaries Using for Loop.py
@@ -0,0 +1,14 @@
+# Example 1: Access both key and value using items()
+
+dt = {'a': 'juice', 'b': 'grill', 'c': 'corn'}
+
+for key, value in dt.items():
+    print(key, value)
+
+
+# Example 2: Access both key and value without using items()
+
+dt = {'a': 'juice', 'b': 'grill', 'c': 'corn'}
+
+for key, value in dt.items():
+    print(key, value)
