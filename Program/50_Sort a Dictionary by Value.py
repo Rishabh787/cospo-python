81429248d161157949f414893a5c80a78773250f
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-03-31 23:25:03 +0530

    exm 2 added

--- Program/50_Sort a Dictionary by Value.py
+++ Program/50_Sort a Dictionary by Value.py
@@ -1,5 +1,17 @@
+# Example 1: Sort the dictionary based on values
+
+
 dt = {5:4, 1:6, 6:3}
 
 sorted_dt = {key: value for key, value in sorted(dt.items(), key=lambda item: item[1])}
 
 print(sorted_dt)
+
+
+
+# Example 2: Sort only the values
+
+dt = {5:4, 1:6, 6:3}
+
+sorted_dt_value = sorted(dt.values())
+print(sorted_dt_value)
