9d811aa5da00ba7d05f6aa18b082682bdeffe1d5
Rishabh Singh <85503050+Rishabh787@users.noreply.github.com>
2023-04-01 20:37:56 +0530

    add example 2

--- Program/51_Check If a List is Empty.py
+++ Program/51_Check If a List is Empty.py
@@ -1,3 +1,13 @@
+# Example 1: Using Boolean operation
+
+
 my_list = []
 if not my_list:
     print("the list is empty")
+
+# Example 2: Using len()
+
+my_list = []
+if not len(my_list):
+    print("the list is empty")
+    
